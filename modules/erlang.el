520f0e21d81a3a037b30c040ea1eac7887f53105
Ranjeet Singh <ranjeet@raeoks.com>
2016-11-27 09:49:17 +0530

    Enable modules in single line

--- modules/erlang.el
+++ modules/erlang.el
@@ -1,14 +1,7 @@
-(nel:require-packages 'auto-complete
-		      'auto-highlight-symbol
-		      'dash
-		      'eproject
-		      'popup
-		      'erlang
-		      'edts)
+(nel:require-packages 'erlang-mode
+		      'erlware-mode)
 
 (nel:run-after-initializing
- (setq edts-inhibit-package-check t)
  (setq erlang-root-dir "/usr/local/lib/erlang")
  (setq exec-path (cons (concat erlang-root-dir "/bin") exec-path))
- (add-hook 'after-init-hook (lambda () (progn (require 'erlang-start)
-					      (require 'edts-start)))))
+ (add-hook 'after-init-hook (lambda () (progn (require 'erlang-start)))))
